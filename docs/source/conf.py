c10a9d03d5be6a051836e3690ca46a4175734c43
Hesham ElAbd <h.elabd@ikmb.uni-kiel.de>
2020-12-02 12:41:35 +0100

    Adjust the conf.py document to be compatable with ReadTheDcos

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,6 +24,7 @@ author = 'Hesham ElAbd'
 # The full version, including alpha/beta/rc tags
 release = '0.4.0'
 
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
