dac3d7a2d81599d9ad8e3ec03c6962f325dffeec
Hesham ElAbd <h.elabd@ikmb.uni-kiel.de>
2021-01-03 21:23:45 +0100

    correcting some typos and doing proof reading for the classes; Proband, Protein, and Tissue

--- library/IPTK/Classes/Proband.py
+++ library/IPTK/Classes/Proband.py
@@ -7,8 +7,8 @@ from IPTK.Utils.UtilityFunction import generate_random_name
 # generate the class 
 class Proband: 
     def __init__(self,**info)->Proband:
-        """initialize a Proband using an arbitrary number of key-value pairs containing information about the proband
-        :return: a proband instane 
+        """create A Proband using an arbitrary number of key-value pairs containing information about the proband
+        :return: A proband instane 
         :rtype: Proband
         """
         self._meta_info=dict()
@@ -19,21 +19,21 @@ class Proband:
             self._meta_info['name']=generate_random_name(12)
     # add meta info 
     def update_info(self, **info)->None:
-        """add new or update existing info about the patient  using an arbitrary number of key-value pair to be added to added to the instance meta-info dict
+        """Add new or update existing info about the patient using an arbitrary number of key-value pairs to be added to the instance meta-info dict
         """
         for key, value in info.items(): 
             self._meta_info[key]=value
             
     def get_name(self)->str:
         """
-        :return: the name of the patient
+        :return: The name of the proband
         :rtype: str
         """
         return self._meta_info['name']
     
     def get_meta_data(self)->dict:
         """
-        :return: a dict that contain all the meta-data about the patient 
+        :return: A dict containing all the meta-data about the proband 
         :rtype: dict
         """
         return self._meta_info
