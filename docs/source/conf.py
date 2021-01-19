2d47e8a6074b67352c039d951c44f3c83bca954b
Hesham ElAbd <h.elabd@ikmb.uni-kiel.de>
2021-01-19 13:20:26 +0100

    Adding some comments for readthedocs main webpage

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('../../library'))
 
 
@@ -31,7 +32,7 @@ master_doc = 'index'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [ 'sphinx.ext.autodoc',]
+extensions = [ 'sphinx.ext.autodoc',"sphinx_rtd_theme"]
 
 #pdf_documents = [('index', u'rst2pdf', u'IPTK Reference', u'Hesham ElAbd'),]
 
@@ -51,7 +52,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'latest'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
