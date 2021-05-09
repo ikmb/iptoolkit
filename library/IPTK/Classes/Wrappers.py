84cf7128191ebccc068c1a2f6d6c35adca0f836a
Hesham ElAbd <heshamelabd@MBP-von-Hesham.fritz.box>
2021-05-09 19:32:04 +0200

    adding wrapper classes and introducing some convient classes to enable easy processing of large number of input files

--- library/IPTK/Classes/Wrappers.py
+++ library/IPTK/Classes/Wrappers.py
@@ -24,7 +24,7 @@ class cExperimet:
         Args:
             filepath (str): the path to load the input file, for example and idXML or an Identification table 
             path2fasta (str): the path to load Fasta database 
-            fileformat (str, optional): type of input format, can be any of idXML, pepXML, mzTab or a CSV Table (IdTable).\
+            fileformat (str, optional): type of input format, can be any of idXML, pepXML, mzTab or a CSV Table.\
                  Defaults to 'idXML'.
             tissue_name (str, optional): The name of the tissue to utilize, this is used for initializing the gene expression table\
                  Defaults to 'total PMBC'.
@@ -39,7 +39,7 @@ class cExperimet:
             raise ValueError(f"The provided path for the identification file : {filepath} does not exist!!")
         if not os.path.exists(path2fasta):
             raise ValueError(f"The path to the proivded fasta file: {path2fasta}, does not exists!!!")
-        if fileformat not in ['idXML', 'pepXML', 'IdTable','mzTab']:
+        if fileformat not in ['idXML', 'pepXML', 'csv','mzTab']:
             raise ValueError(f"Unknow input format, the provided format: {fileformat} is not supported, currently supported values are: {', '.join(['idXML', 'pepXML', 'IdTable','mzTab'])}")
         # define the data 
         self._proband = Proband(name=proband_name)  # the name of the proband 
@@ -93,7 +93,7 @@ class cExperimet:
         """
         self._cashed_results[results_name]=results_value
 
-class PairedExperiment:
+class ReplicatedExperiment:
     def __init__(self,path:Union[str,os.path], anchor_name:str ,path2fasta:str,
         fileformat:str='idXML', tissue_name:str='total PMBC', proband_name:str='Default Proband',
         hla_set:List[str]=['DRB1*15:01','DRB1*15:01'])->PairedExperiment:
@@ -195,6 +195,164 @@ class PairedExperiment:
         """append results to the instance hash map of results.
         Args:
             result_name (str): the name
-            result_value (object): [description]
+            result_value (object): the results to be associated with the provided name 
         """
         self._cashed_results[result_name]=result_value
+    
+class cExperimentSet:
+    def __init__(self,path:Union[str,os.path],path2fasta:str,
+        fileformat:str='idXML', tissue_name:str='total PMBC', proband_name:str='Default Proband',
+        hla_set:List[str]=['DRB1*15:01','DRB1*15:01'])->cExperimentSet:
+        """Construction an ExperimentSet from all the input identification table
+
+        Args:
+            path (Union[str,os.path]): The path to the root directory. 
+            path2fasta (str): the path to load Fasta database 
+            fileformat (str, optional): type of input format, can be any of idXML, pepXML, mzTab or a CSV Table (IdTable).\
+                 Defaults to 'idXML'.
+            tissue_name (str, optional): The name of the tissue to utilize, this is used for initializing the gene expression table\
+                 Defaults to 'total PMBC'.
+            proband_name (str, optional): the name of the proband from whome the data was obtained. Defaults to 'Default Proband'.
+            hla_set (List[str], optional): A list of HLA alleles from whome the data was obtained. Defaults to ['DRB1*15:01','DRB1*15:01'].
+
+        Raises:
+            ValueError: [description]
+
+        Returns:
+            cExperimentSet: [description]
+        """
+        ## Check that the path exists 
+        if not os.path.exists(path):
+            raise ValueError(f"The provided root directory: {path} does not exists!")
+        # Get the filenames relative to the path base 
+        filenames=[os.path.join(path,name) for name in os.listdir(path) if fileformat in name] 
+        # Check that the path exists  
+        if len(filenames)==0:
+            raise ValueError(f"Could not find any file in the provided directory: {path} with the provided format: {fileformat}")
+        # build the collection of experiments 
+        exps=dict()
+        try:
+            for file_name in tqdm(filenames):
+                parsed_name=file_name.split('/')[-1].split('.')[0]
+                experiment=cExperimet(file_name, path2fasta, fileformat, tissue_name, proband_name, hla_set)
+                exps[parsed_name]=experiment
+        except Exception as exp:
+            raise RuntimeError(f"Creating an Experimental instance from the file: {file_name} fialed with the following error \n{str(exp)}\n")
+        self._exps=ExperimentSet(exps)
+        self._cashed_results=dict()
+        return 
+
+    def get_experiments(self)->ExperimentSet:
+        """ 
+        Returns:
+            ExperimentSet: The instance ExperimentalSet 
+        """
+        return self._exps
+
+    def cash_results(self, result_name:str,result_value:object):
+        """append results to the instance hash map of results.
+        Args:
+            result_name (str): the name
+            result_value (object): the results to be associated with the provided name 
+        """
+        self._cashed_results[result_name]=result_value
+        
+class ReplicatedExperimentSet:
+    def __init__(self,path:Union[str,os.path],path2fasta:str,
+        fileformat:str='idXML', tissue_name:str='total PMBC', proband_name:str='Default Proband',
+        hla_set:List[str]=['DRB1*15:01','DRB1*15:01'])->ReplicatedExperimentSet:
+        """
+        Args:
+            path (Union[str,os.path]): The path to the root directory where files are located
+            path2fasta (str): the path to load Fasta database 
+            fileformat (str, optional): type of input format, can be any of idXML, pepXML, mzTab or a CSV Table (IdTable).\
+                 Defaults to 'idXML'.
+            tissue_name (str, optional): The name of the tissue to utilize, this is used for initializing the gene expression table\
+                 Defaults to 'total PMBC'.
+            proband_name (str, optional): the name of the proband from whome the data was obtained. Defaults to 'Default Proband'.
+            hla_set (List[str], optional): A list of HLA alleles from whome the data was obtained. Defaults to ['DRB1*15:01','DRB1*15:01'].            
+
+        Returns:
+            ReplicatedExperimentSet: a collection of replicated Experiments. 
+        """
+        ## Check that file name exists 
+        if not os.path.exists(path):
+            raise ValueError(f"The provided path: {path}, does not exists")
+        ## list file names:
+        filenames=[os.path.join(path,name) for name in os.listdir(path) if fileformat in name] 
+        ## load the file names 
+        if len(filenames)==0:
+            raise ValueError(f"Could not find any file in the provided directory: {path} with the provided format: {fileformat}")
+        ## get experimental names:
+        replicates=self._group_paired_files(filenames)
+        ## allocate a container to hold the results 
+        self._results=dict()
+        try:
+            for replicate in tqdm(replicates):
+                anchor_name=self._get_common_prefix(replicate[0],replicate[1])
+                exp=ReplicatedExperiment(path, anchor_name, path2fasta, fileformat, tissue_name, proband_name, hla_set)
+                self._results[anchor_name]=exp
+        except Exception as exp:
+            raise ValueError(f"While generating a RepeatedExperiment from the following file: {', '.join(replicate)}, the following error was Encountered: \n{exp}\n")
+        return 
+
+    def _group_paired_files(self, file_names:List[str])->List[[str]]:
+        """ takes a list of input files and returns a list of lists where each child list contains replicate files
+
+        Args:
+            file_names (List[str]): a collection of file names 
+
+        Returns:
+            List[[str]]: a list of lists where each child list contains replicate files
+        """
+        results=[]
+        for file_name_outer in file_names:
+            for file_name_inter in file_names:
+                if self._are_replicates(file_name_outer,file_name_inter):
+                    results.append([file_name_outer,file_name_inter])
+        return results 
+
+    def _are_replicates(filename_1:str, filename_2:str)->bool:
+        """a helper function that check whether two files are replicate or not be comparing their names
+
+        Args:
+            filename_1 (str): The filename of the first experiment 
+            filename_2 (str): The filename of the second experiment 
+
+        Returns:
+            bool: True if the two filenames are identical strings that only differ in one charachter and False otherwise. 
+        """
+        if len(str_1)!=len(str_2):return False 
+        diff=[]
+        for a,b in zip(str_1,str_2):
+            if a!=b:
+                diff.append([a,b])
+            if len(diff)>1:
+                return False
+        return True if len(diff)==1 else False
+
+    def _get_common_prefix(file_name1:str,file_name2:str)->str:
+        """take an equal length file names and return the shared prefix between both files 
+
+        Args:
+            file_name1 (str): The name of the first file 
+            file_name2 (str): The name of the second file 
+
+        Returns:
+            str: the prefix string between both files 
+        """
+        if len(file_name1)!=len(file_name2):
+            raise ValueError(f"Provided String MUST be of equal length, found first file to have a length of {en(file_name1)} while the second has a length of {en(file_name2)}")
+        idx=0
+        for a, b in zip(file_name1,file_name2):
+            if a!=b:
+                break
+            idx+=1
+        return file_name1[:idx]
+
+    def get_experiments(self)->Dict[str,ReplicatedExperiment]:
+        """
+        Returns:
+            Dict[str,ReplicatedExperiment]: a dict containg a collection of ReplicatedExperiments  
+        """
+        return self._results
